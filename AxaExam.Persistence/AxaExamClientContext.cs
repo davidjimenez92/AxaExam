dcabd63efa92664203418b8ecc60504800d9ee62
David Jimenez <davidjimenez92@gmail.com>
2020-03-27 18:28:54 +0100

    Persistence to Clients close #2

---
+++ AxaExam.Persistence/AxaExamClientContext.cs
@@ -0,0 +1,19 @@
+﻿using AxaExam.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxaExam.Persistence
+{
+	public class AxaExamClientContext: DbContext
+	{
+		public DbSet<Client> Clients { get; set; }
+
+		public AxaExamClientContext() : base("")
+		{
+		}
+	}
+}
